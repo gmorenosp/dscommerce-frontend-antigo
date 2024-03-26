20133aa094be92ff0f75285266b86c7067474d1c
Gustavo Moreno <gmorenosmail@gmail.com>
2024-03-26 10:20:08 -0300

    Requisicoes para o catalodo e por id

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -3,9 +3,23 @@ import SearchBar from '../../../components/SearchBar';
 import CatalogCard from '../../../components/CatalogCard';
 import ButtonNextPage from '../../../components/ButtonNextPage';
 import * as productService from '../../../services/product-service';
+import { useEffect, useState } from 'react';
+import { ProductDTO } from '../../../models/product';
+import axios from 'axios';
 
 
 export default function Catalog() {
+  
+  const[products, setProducts] = useState<ProductDTO[]>([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/products?size=12")
+      .then(response => {
+          setProducts(response.data.content);
+      });
+  }, []);
+
+
   return (
    
     <main>
@@ -13,9 +27,8 @@ export default function Catalog() {
         <SearchBar />
 
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-          
           {
-            productService.findAll().map(
+            products.map(
                 product => <CatalogCard key={product.id} product={product} />)
           }
         </div>
