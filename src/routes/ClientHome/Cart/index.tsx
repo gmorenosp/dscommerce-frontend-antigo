9c153a096604253417121fa3069064b423479fb4
Gustavo Moreno <gmorenosmail@gmail.com>
2024-03-27 04:45:08 -0300

    Cart save/get

--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -1,5 +1,5 @@
 import "./styles.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import * as cartService from '../../../services/cart-service';
 import { OrderDTO, OrderItemDTO } from "../../../models/order";
 
@@ -15,17 +15,8 @@ const item2 : OrderItemDTO = new OrderItemDTO(
 
 export default function Cart() {
 
-  const cart : OrderDTO = new OrderDTO(); 
+  const [cart, setCart] = useState<OrderDTO>(cartService.getCart());
 
-    useEffect(() => {
-      
-      cart.items.push(item1);
-      cart.items.push(item2);
-
-      cartService.saveCart(cart);
-
-
-    }, []);
 
   return (
     <main>
--- src/services/cart-service.ts
+++ src/services/cart-service.ts
@@ -3,5 +3,8 @@ import * as cartRepositoy from '../localstorage/cart-repository';
 
 export function saveCart(cart: OrderDTO) {
     cartRepositoy.save(cart);
+}
 
+export function getCart() : OrderDTO {
+    return cartRepositoy.get();
 }
